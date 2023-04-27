50a2506e3f497b1986b93a64a813f6be85e69226
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2023-04-27 16:22:49 -0400

    trying to figure out how tf slash commands work

--- src/Main.hx
+++ src/Main.hx
@@ -1,6 +1,5 @@
 package;
 
-
 import tokens.Token;
 import hxdiscord.DiscordClient;
 import hxdiscord.utils.Intents;
@@ -19,13 +18,17 @@ class Main
     public static function onReady()
     {
         trace("The bot is ready");
+		if (sys.FileSystem.exists("src/commands/commands.json"))
+			Bot.setInteractionCommands(haxe.Json.parse(sys.io.File.getContent("src/commands/commands.json")));
+        else
+            haxe.macro.Context.error('Failed to load json', haxe.macro.Context.currentPos());
     }
 
     public static function onMessageCreate(m:Message)
     {
         if (m.content == "hi")
-        {
-            m.reply({content:"Hello!"}, true); //true or false, the bot will mention the user
-        }
-    }
+            m.reply({content:"Hello!"}, true);
+        else if (m.content == ":doblez:")
+            m.reply({content:":doblez:"}, true);
+}
 }
